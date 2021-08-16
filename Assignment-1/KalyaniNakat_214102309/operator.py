112bceb6e9626a80a40615e0b6ff5a7f44647e5d
knakat1998 <89026851+knakat1998@users.noreply.github.com>
2021-08-16 20:08:49 +0530

    Add files via upload

---
+++ Assignment-1/KalyaniNakat_214102309/operator.py
@@ -0,0 +1,12 @@
+a = float(input(" enter first input num: "))
+b = float(input(" enter second input num: "))
+c = a + b
+d = a - b
+e = a * b
+f = a / b
+g = a % b
+print(c)
+print(d)
+print(e)
+print(f)
+print(g)
